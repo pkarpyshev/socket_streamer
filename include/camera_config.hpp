9807ad62e2320667151bd26e4755c88f270df26c
Ildar Babataev <ildaralaric1998@gmail.com>
2022-02-14 13:10:55 +0300

    Test bigger resolution

--- include/camera_config.hpp
+++ include/camera_config.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#define CAM_MODE    7
-#define CAM_WIDTH   640
-#define CAM_HEIGHT  480
+#define CAM_MODE    6
+#define CAM_WIDTH   1280
+#define CAM_HEIGHT  720
 #define CAM_FPS     90
